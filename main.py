4eaa1fb0dbe04b818580a0a1e4630a5cb1436640
unknown <=>
2014-12-13 20:43:55 -0500

    After first submission

--- main.py
+++ main.py
@@ -54,41 +54,54 @@ trainData = np.ones((rowCount-1,colCount-3))
 for count in range(1,rowCount):
 	targetC[count-1] = int(data[count][colCount-3])
 	targetR[count-1] = int(data[count][colCount-2])
-	for c2 in range(1,colCount-3):
+	for c2 in range(2,colCount-3):
 		trainData[count-1][c2] = data[count][c2]
 	dt = data[count][0].rsplit(" ")
+	monthnday = dt[0].split("-")
 	trainData[count-1][0] = int(dt[1].rsplit(":")[0])
+	trainData[count-1][1] = int(monthnday[0]) + float(monthnday[1])/30
 
 
 #################################### kfold ################################
 K = rowCount/2
+step = 2
 # for iter in range(0, rowCount/K) :
 	# startIndex = iter*K
 	# if((iter+1)*K
-print K
-fakeTest = matCopy(trainData, 0,K)
-fakeTestTargetC = targetC[0:K]
-fakeTestTargetR = targetR[0:K]
+# fakeTest = matCopy(trainData, 0,K)
+# fakeTestTargetC = targetC[0:K]
+# fakeTestTargetR = targetR[0:K]
+# fakeTestTarget = map(add,fakeTestTargetR,fakeTestTargetC)
+
+# fakeTrain = trainData[K:rowCount]
+# fakeTrainTargetC = targetC[K:rowCount]
+# fakeTrainTargetR = targetR[K:rowCount]
+
+fakeTest = trainData[0:rowCount:step]
+fakeTestTargetC = targetC[0:rowCount:step]
+fakeTestTargetR = targetR[0:rowCount:step]
 fakeTestTarget = map(add,fakeTestTargetR,fakeTestTargetC)
 
-fakeTrain = trainData[K:rowCount]
-fakeTrainTargetC = targetC[K:rowCount]
-fakeTrainTargetR = targetR[K:rowCount]
+fakeTrain = trainData[1:rowCount:step]
+fakeTrainTargetC = targetC[1:rowCount:step]
+fakeTrainTargetR = targetR[1:rowCount:step]
 
 
 ################################### Load actual test ################################
-# reader = csv.reader(open(testFilePath),delimiter=",")
-# testd = list(reader)
-# colCount = len(testd[1])
-# rowCount = len(testd)
-# testTargetID = []
-# testData = np.ones((rowCount-1,colCount))
-# for count in range(1,rowCount):
-	# for c2 in range(1,colCount):
-		# testData[count-1][c2] = testd[count][c2]
-	# dt = testd[count][0].rsplit(" ")
-	# testData[count-1][0] = int(dt[1].rsplit(":")[0])
-	# testTargetID.append(testd[count][0])
+reader = csv.reader(open(testFilePath),delimiter=",")
+testd = list(reader)
+colCount = len(testd[1])
+rowCount = len(testd)
+testTargetID = []
+testData = np.ones((rowCount-1,colCount))
+for count in range(1,rowCount):
+	for c2 in range(2,colCount):
+		testData[count-1][c2] = testd[count][c2]
+	dt = testd[count][0].rsplit(" ")
+	monthnday = dt[0].split("-")
+	testData[count-1][0] = int(dt[1].rsplit(":")[0])
+	testData[count-1][1] = int(monthnday[0]) + float(monthnday[1])/30
+	testTargetID.append(testd[count][0])
 
 ################################### Process #####################################
 # colToSkip = normalize(trainData)
@@ -110,9 +123,7 @@ fakeTrainTargetR = targetR[K:rowCount]
 # GNBclf = GaussianNB()
 # GNBprediction = GNBclf.fit(maskedTrD,target).predict(maskedTD)
 
-# Tclf = tree.DecisionTreeClassifier()
-# Tclf.fit(maskedTrD,target)
-# Tprediction = Tclf.predict(maskedTD)
+
 
 # SGDclf = SGDClassifier(loss="log")
 # SGDclf.fit(maskedTrD,target)
@@ -123,40 +134,64 @@ fakeTrainTargetR = targetR[K:rowCount]
 # KNclf.fit(maskedTrD,target)
 # KNprediction = KNclf.predict(maskedTD)
 
-# RCFclf = RandomForestClassifier(n_estimators=300,max_features=7)
-# RCFprediction = RCFclf.fit(maskedTrD,target).predict(maskedTD)
+#GDBclf = GradientBoostingClassifier(n_estimators = 100)
+#GDBprediction = GDBclf.fit(maskedTrD,target).predict(maskedTD)
 
 RCFclf = RandomForestClassifier(n_estimators=10,max_features=9)
 RCFpredictionC = RCFclf.fit(fakeTrain,fakeTrainTargetC).predict(fakeTest)
 RCFpredictionR = RCFclf.fit(fakeTrain,fakeTrainTargetR).predict(fakeTest)
 RCFprediction = map(add, RCFpredictionR, RCFpredictionC)
-print RCFprediction[0]
+
+Tclf = tree.DecisionTreeClassifier()
+TclfpredictionC = Tclf.fit(fakeTrain,fakeTrainTargetC).predict(fakeTest)
+TclfpredictionR = Tclf.fit(fakeTrain,fakeTrainTargetR).predict(fakeTest)
+Tclfprediction = map(add,TclfpredictionR,TclfpredictionC)
+
+ETclf = ExtraTreesClassifier(n_estimators=4)
+ETclfpredictionC = ETclf.fit(fakeTrain,fakeTrainTargetC).predict(fakeTest)
+ETclfpredictionR = ETclf.fit(fakeTrain,fakeTrainTargetR).predict(fakeTest)
+ETclfprediction = map(add,ETclfpredictionR,ETclfpredictionC)
+
+##################################### Actual Classify ####################################
 
 # RCFclf = RandomForestClassifier(n_estimators=10,max_features=9)
-# RCFprediction = RCFclf.fit(trainData,target).predict(testData)
+# prediction = RCFclf.fit(trainData,target).predict(testData)
 
-# ETclf = ExtraTreesClassifier(n_estimators=200)
-# ETprediction = ETclf.fit(maskedTrD,target).predict(maskedTD)
+# ETclf = ExtraTreesClassifier(n_estimators=4)
+# predictionC = ETclf.fit(trainData,targetC).predict(testData)
+# predictionR = ETclf.fit(trainData,targetR).predict(testData)
+# prediction = map(add,predictionC,predictionR)
 
-#GDBclf = GradientBoostingClassifier(n_estimators = 100)
-#GDBprediction = GDBclf.fit(maskedTrD,target).predict(maskedTD)
+Tclf = tree.DecisionTreeClassifier()
+TclfpredictionC = Tclf.fit(trainData,targetC).predict(testData)
+TclfpredictionR = Tclf.fit(trainData,targetR).predict(testData)
+prediction = map(add,TclfpredictionR,TclfpredictionC)
+
 ##################################### Evaluate ####################################
 
-numCorr = 0
-ii = 0
-total = 0
-for ii in range(0,len(fakeTestTargetC)) :
-	total += math.pow(math.log(RCFprediction[ii]+1)-math.log(fakeTestTarget[ii]+1),2)
-accuracy = math.sqrt(total/len(fakeTestTargetC))
-print("RCF's Accuracy :",accuracy)
+# numCorr = 0
+# ii = 0
+# totalR = 0
+# totalT = 0
+# totalE = 0
+# for ii in range(0,len(fakeTestTargetC)) :
+	# totalR += math.pow(math.log(RCFprediction[ii]+1)-math.log(fakeTestTarget[ii]+1),2)
+	# totalT += math.pow(math.log(Tclfprediction[ii]+1)-math.log(fakeTestTarget[ii]+1),2)
+	# totalE += math.pow(math.log(ETclfprediction[ii]+1)-math.log(fakeTestTarget[ii]+1),2)
+# accuracyR = math.sqrt(totalR/len(fakeTestTargetC))
+# accuracyT = math.sqrt(totalT/len(fakeTestTargetC))
+# accuracyE = math.sqrt(totalE/len(fakeTestTargetC))
+# print("RCF's Accuracy :",accuracyR)
+# print("ET's Accuracy :",accuracyE)
+# print("T's Accuracy :",accuracyT)
 
 
 ############################## write output ###################################
-# with open(outPath,'wb') as csvFile:
-   # writer = csv.writer(csvFile,delimiter = ',')
-   # writer.writerow(['datetime','count'])
-   # for ii in range(0,len(fakeTestTargetC)):
-       # writer.writerow([testTargetID[ii], RCFprediction[ii]])
+with open(outPath,'wb') as csvFile:
+   writer = csv.writer(csvFile,delimiter = ',')
+   writer.writerow(['datetime','count'])
+   for ii in range(0,len(prediction)):
+       writer.writerow([testTargetID[ii], prediction[ii]])
 
 ############################## Confusion Matrix ###############################
 # import matplotlib.pyplot as plt
